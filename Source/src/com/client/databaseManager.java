f1b6d5b384ac18736bdc9381ca6b4dc171da4904
James <sitzja@grizzlies.adams.edu>
2013-05-14 17:24:43 -0600

    Created Person Object Started Work on Database Manager

---
+++ Source/src/com/client/databaseManager.java
@@ -0,0 +1,19 @@
+package com.client;
+
+import java.util.*;
+
+public class databaseManager implements Runnable
+{
+	List<Person> asuPeople;
+	Thread managerThread;
+	boolean isRunning;
+	
+	public void run()
+	{
+		while(isRunning)
+		{
+			//Do running code
+		}
+	}
+	
+}
