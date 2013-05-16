19399e643a6effe03d874f6b6b939ca4aa34eef9
James <sitzja@grizzlies.adams.edu>
2013-05-15 22:18:09 -0600

    Major code write. Settings logic is implemented.

--- Source/src/com/client/richardson/prog.java
+++ Source/src/com/client/richardson/prog.java
@@ -2,6 +2,9 @@ package com.client.richardson;
 
 import java.io.*;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 public class prog 
 {
 	//Start Database Manager
@@ -11,40 +14,73 @@ public class prog
 	//how are you?
 	public static void main(String[] args)
 	{
-		try
+		File settings = new File("./properties.prop");
+		if(settings.exists())
 		{
-			FileInputStream fStream = new FileInputStream("./properties.prop");
-			DataInputStream dis = new DataInputStream(fStream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(dis));
-			
-			String settingLine;
-			while((settingLine = br.readLine()) != null)
+			try
 			{
-				//Read Settings
-				String setting = "";				
-				int index = 0;
-				while(settingLine.charAt(index) != ':')
+				FileInputStream fStream = new FileInputStream(settings);
+				DataInputStream dis = new DataInputStream(fStream);
+				BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+		
+				String settingLine;
+				while((settingLine = br.readLine()) != null)
 				{
-					setting += settingLine.charAt(index);
-					index++;
-				}
-				if(setting.toUpperCase().equals("DATABASE"))
-				{
-					//Read in Database configuration
-				}
-				else
-				{
-					if(setting.toUpperCase().equals("PERSONS"))
+					//Read Settings
+					String setting = "";				
+					int index = 0;
+					while(settingLine.charAt(index) != ':')
 					{
-						//Read Persons File location
+						setting += settingLine.charAt(index);
+						index++;
+					}
+					if(setting.toUpperCase().equals("DATABASE"))
+					{
+						//Read in Database configuration
+					}
+					else
+					{
+						if(setting.toUpperCase().equals("PERSONS"))
+						{
+							//Read Persons File location
+						}
 					}
 				}
 			}
+			catch(Exception e)
+			{
+				//Do nothing
+			}
 		}
-		catch(Exception e)
+		else
 		{
-			//Probably means file doesn't exist, start to set up mode
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Settings File Not Found.");
+			
+			JFileChooser fc = new JFileChooser();
+			fc.showDialog(null, "SELECT");
+			fc.setFileFilter(null);
+			File database = fc.getSelectedFile();
+			fc.showDialog(null, "SELECT");
+			File persons = fc.getSelectedFile();
+			
+			try 
+			{
+				if(settings.createNewFile())
+				{
+					FileWriter fOutput = new FileWriter(settings);
+					BufferedWriter bw = new BufferedWriter(fOutput);
+					bw.write("DATABASE:" + database.getAbsolutePath() + "\n");
+					bw.write("PERSONS:" + persons.getAbsolutePath() + "\n");
+					bw.close();
+					fOutput.close();
+				}
+			} 
+			catch (Exception e) 
+			{
+				//Ignore the exceptions
+			}
+			
+			
 		}
 	}
 }
